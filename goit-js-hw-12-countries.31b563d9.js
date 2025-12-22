9ce84ac1a64022fa1a49e3678fe0980365137e24
NastifyMe <suprunar2010@gmail.com>
2025-12-22 12:47:05 +0100

    Updates

--- goit-js-hw-12-countries.31b563d9.js
+++ goit-js-hw-12-countries.31b563d9.js
@@ -714,6 +714,21 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 }
 
 },{}],"a0t4e":[function(require,module,exports,__globalThis) {
+function listCountry(countries) {
+    return `
+    <ul>
+        ${countries.map((country)=>`<li>${country.name}</li>`).join('')}
+    </ul>`;
+}
+function countryCard(country) {
+    return `
+    <h2>${country.name}</h2>
+    <p><b>Capital:</b>${country.capital}</p>
+    <p><b>Population</b>${country.population}</p>
+    <p><b>languages:</b>
+    ${country.languages.map((language)=>language.name)}</p>
+    <img src="${country.flag}" alt="">`;
+}
 
 },{}]},["5j6Kf","a0t4e"], "a0t4e", "parcelRequirec490", {})
 
